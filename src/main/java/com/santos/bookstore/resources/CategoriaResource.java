9cb85e487b5866c574df8e63496416d905927fcb
Rafael Santos <leafarccomputaca@gmail.com>
2022-06-25 19:30:47 -0300

    Find All para categoria utilizando Padrão DTO

--- src/main/java/com/santos/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/santos/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.santos.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.santos.bookstore.domain.Categoria;
+import com.santos.bookstore.dtos.CategoriaDTO;
 import com.santos.bookstore.services.CategoriaService;
 
 @RestController
@@ -25,6 +29,12 @@ public class CategoriaResource {
 		return ResponseEntity.ok(obj);
 	}
 	
-	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findall(){
+		List<Categoria> list = categoriaService.encontraTodasCategorias();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
 	
 }
--- src/main/java/com/santos/bookstore/services/CategoriaService.java
+++ src/main/java/com/santos/bookstore/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.santos.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,9 @@ public class CategoriaService {
 				"Objeto não econtrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	
+	public List<Categoria>encontraTodasCategorias(){
+		return categoriaRepository.findAll();
+	}
+	
 }
