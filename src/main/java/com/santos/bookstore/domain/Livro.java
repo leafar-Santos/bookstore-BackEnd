1ed4ed5fe85f34d61ed444bb771f16e624ed3274
Rafael Santos <leafarccomputaca@gmail.com>
2022-06-25 17:12:29 -0300

    classes de domínio iniciadas

---
+++ src/main/java/com/santos/bookstore/domain/Livro.java
@@ -0,0 +1,88 @@
+package com.santos.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+
+	private Integer id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+
+	// Um livro tem obrigatóriamente uma categoria.
+	private Categoria categoria;
+
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Livro(Integer id, String titulo, String nomeAutor, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nomeAutor = nomeAutor;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoria, id, nomeAutor, texto, titulo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(categoria, other.categoria) && Objects.equals(id, other.id)
+				&& Objects.equals(nomeAutor, other.nomeAutor) && Objects.equals(texto, other.texto)
+				&& Objects.equals(titulo, other.titulo);
+	}
+
+}
